47caf49eb2fc6c34c681509132446ee5a063fff0
Prime <adilkaiser24@gmail.com>
2024-11-14 19:37:50 +0500

    create blog page created + app completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./Home"
 import BlogDetails from "./BlogDetails"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Navbar from "./Navbar"
+import Create from "./Create"
 
 function App() {
 
@@ -12,6 +13,9 @@ function App() {
         <Switch>
         <Route exact path="/">
           <Home />
+        </Route>
+        <Route exact path="/create">
+          <Create />
         </Route>
         <Route path="/blogs/:id">
           <BlogDetails />
