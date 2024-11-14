b18f4c7449c32d9e960b046d5d2c8953b445c4f7
Prime <adilkaiser24@gmail.com>
2024-11-14 18:55:33 +0500

    blog details page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import Home from "./Home"
+import BlogDetails from "./BlogDetails"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 function App() {
 
   return (
-    <>
-    <Home />
-    </>
+    <Router>
+      <div className="content">
+        <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/blogs/:id">
+          <BlogDetails />
+        </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
